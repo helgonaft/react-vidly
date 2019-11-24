1301c1a2675af98a1e05238b109d6971ec97d9d1
Olha Yegorova <OYEGOROVA@pier1.com>
2019-11-24 22:29:42 +0200

    sorting: unique keys

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -6,14 +6,19 @@ class TableBody extends Component {
     if (column.content) return column.content(item);
     return _.get(item, column.path);
   };
+  createKey = (item, column) => {
+    return item._id + (column.path || column.key);
+  };
   render() {
     return (
       <tbody>
         {this.props.data.map((item, index) => {
           return (
             <tr key={index}>
-              {this.props.columns.map((column, index) => (
-                <td>{this.renderCell(item, column)}</td>
+              {this.props.columns.map(column => (
+                <td key={this.createKey(item, column)}>
+                  {this.renderCell(item, column)}
+                </td>
               ))}
             </tr>
           );
