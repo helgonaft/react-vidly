9d6d92587cdb221b4d7e3ef5567365f859d346b8
Olga <helgonaft@gmail.com>
2019-11-28 19:59:33 +0200

    basic input validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import Input from "./common/input";
 
 class LoginForm extends Component {
-  //username = React.createRef();
+  state = { account: { username: "", password: "" }, errors: {} };
 
-  componentDidMount() {
-    //this.username.current.focus();
-  }
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "") {
+      errors.username = "Username is required";
+    }
+    if (account.password.trim() === "") {
+      errors.password = "Password is required";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
   handleSubmit = e => {
     // stop submittig the form which causes full page reload
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
     // call the server
   };
 
@@ -19,9 +31,9 @@ class LoginForm extends Component {
     account[e.currentTarget.name] = e.currentTarget.value;
     this.setState({ account });
   };
-  state = { account: { username: "", password: "" } };
+
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -30,12 +42,14 @@ class LoginForm extends Component {
             name="username"
             label="Username"
             value={account.username}
+            error={errors.username}
             onChange={this.handleChange}
           />
           <Input
             name="password"
             label="Password"
             value={account.password}
+            error={errors.password}
             onChange={this.handleChange}
           />
           <button type="submit" className="btn btn-primary">
